8c22646634ff33688947a89d42e7ee51b05fa99e
micksarin <micksarin@gmail.com>
2026-03-13 10:10:23 +0700

    revised:ex-4 answer

--- ex-4.js
+++ ex-4.js
@@ -8,16 +8,20 @@ const employees = [
     name: "Alex",
     age: 25,
     hobbies: ["Cryptocurrency", "Social media"],
-  },
-  {
-    name: "Alicia",
-    age: 29,
-    hobbies: ["Shopping","Reading novels"],
-  },{
-    name: "Kody",
-    age: 19,
-    hobbies: ["Computer games","Wakeboard"],
-  },
+  }
 ];
 
 // Start coding here
+employees.push({
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+});
+
+employees.push({
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"],
+});
+
+console.log(employees);
